d9a25070b4f0112f7352669ac0590672467ac1c1
maxline <seleon000@gmail.com>
2019-06-05 10:14:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Company UP XML parser app
---
+++ src/main/java/com/companyup/companyupparser/CompanyupParserApplication.java
@@ -0,0 +1,13 @@
+package com.companyup.companyupparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyupParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyupParserApplication.class, args);
+    }
+
+}
